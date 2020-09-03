52594e875e0587714707f3fe52b9b21374630376
Steven De Moor <demoorsteven@yahoo.com>
2020-09-03 16:16:25 +0200

    Go

--- env.bat
+++ env.bat
@@ -1,10 +1,9 @@
 set MYSQL_USER=root
 set MYSQL_PASSWORD=stevendemoor
 set MYSQL_PREFIX=cocktailito_
-set NODE_ENV=developpement
+set NODE_ENV=development
 
-REM export MYSQL_USER=root
-REM export MYSQL_PASSWORD=stevendemoor
-REM export MYSQL_PREFIX=cocktailito_
-REM export NODE_ENV=developpement
+REM export MYSQL_USER=ekoffbcroot
+REM export MYSQL_PASSWORD=Auditeks1
+REM export NODE_ENV=production
 REM printenv to check
